07b781c7febb156fe8c58484fdc4815ab739876f
Shlomo Kabareti <shlomo@plasmapos.com>
2026-09-01 21:18:58 -0400

    feat: add convexAuthOrganizations component with core child dependency

--- packages/auth/src/component/organizations/schema.ts
+++ packages/auth/src/component/organizations/schema.ts
@@ -1,38 +1,14 @@
 import { defineSchema } from "convex/server";
-import { auth_identities, users } from "../schema/users.js";
 import {
-  organizations,
   organization_invitations,
   organization_members,
   organization_roles,
+  organizations,
 } from "../schema/organizations.js";
-import {
-  authAccounts,
-  authMagicLinkTokens,
-  authRateLimits,
-  authRefreshTokens,
-  authSessions,
-  authVerificationCodes,
-  authVerifiers,
-} from "../schema/native.js";
-import { api_keys, auth_audit_events } from "../schema/apiKeys.js";
-import { service_principals } from "../schema/servicePrincipals.js";
 
 export default defineSchema({
-  users,
-  auth_identities,
-  authAccounts,
-  authSessions,
-  authRefreshTokens,
-  authVerificationCodes,
-  authVerifiers,
-  authRateLimits,
-  authMagicLinkTokens,
   organizations,
   organization_roles,
   organization_members,
   organization_invitations,
-  api_keys,
-  auth_audit_events,
-  service_principals,
 });
